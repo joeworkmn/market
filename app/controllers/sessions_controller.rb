de71fcf2110f266d24137f39929cec2691472d24
Joseph Workman <joeworkmn@gmail.com>
2014-03-25 16:30:01 -1000

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
    end
 
    def create
+      user = User.find_by(username: params[:session][:username])
+      if user && user.authenticate(params[:session][:password])
+         flash[:success] = "Welcome!"
+      else
+         flash[:alert] = "Incorrect username or password"
+      end
+      redirect_to new_session_path
    end
 
    def destroy
