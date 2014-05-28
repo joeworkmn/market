df45d6c2f3cf9ef99ad4bfdb667aa17fb8ee7bc2
Joseph Workman <joeworkmn@gmail.com>
2014-05-27 17:41:02 -1000

    Test placing bids and buyouts in auctions#show view

--- test/integration/auction_pages_test.rb
+++ test/integration/auction_pages_test.rb
@@ -10,35 +10,68 @@ class AuctionPagesTest < ActionDispatch::IntegrationTest
 
    describe "Show" do
       let(:auction) { create(:auction) }
-      let(:user) { create(:user) }
+      let(:user)    { create(:user) }
 
-      describe "Before signed in" do
-         before { visit auction_path auction }
-
-         it { wont_have_button "Buy Out" }
-         it { wont_have_button "Place starting bid" }
-         it { wont_have_text "Enter your bid:" }
+      before do
+         sign_in(user)
+         visit auction_path auction
       end
 
-
-      describe "After signed in" do
+      describe "When not signed in" do
          before do
-            sign_in(user)
+            sign_out
             visit auction_path auction
          end
 
-         it { must_have_button "Buy Out" }
-         it { must_have_button "Place starting bid" }
-         it { must_have_text "Enter your bid:" }
+         it { wont_have_button "Buy Out" }
+         it { wont_have_button "Place starting bid" }
+         it { wont_have_text "Or enter your bid:" }
+      end
 
-         describe "After placing starting bid" do
-            describe "When using increment button" do
-               before { click_button "Place starting bid" }
+      it { must_have_button "Buy Out" }
+      it { must_have_button "Place starting bid" }
+      it { must_have_text "Or enter your bid:" }
 
-               it { must_have_button "Place a bid of: $#{auction.start_bid + auction.bid_increment}" }
+      describe "After placing starting bid" do
+         before { click_button "Place starting bid" }
+
+         it { must_have_button "Place a bid of: $#{auction.start_bid + auction.bid_increment}" }
+      end
+
+      describe "When placing a bid" do
+         describe "From bid form" do
+            before { bid_from_form(60) }
+
+            it "Must show the high bid" do
+               within(".auction-info") { page.must_have_text("High bid: $60.00") }
+               within(".high-bid")     { page.must_have_text("High bid: $60.00") }
             end
+
+            it { must_have_button "Place a bid of: $#{60 + auction.bid_increment}" }
+         end
+
+         describe "From increment button" do
+            before do
+               click_increment_button # To place starting bid.
+               @bid = increment_button_amount
+               click_increment_button
+            end
+
+            it "Must show the high bid" do
+               within(".auction-info") { page.must_have_text("High bid: $#{@bid}") }
+               within(".high-bid")     { page.must_have_text("High bid: $#{@bid}") }
+            end
+
+            it { must_have_button "Place a bid of: $#{@bid + auction.bid_increment}" }
          end
       end
 
+      describe "When auction is bought out" do
+         it "Must close the auction" do
+            click_buyout_button
+            page.must_have_text "You have won the auction"
+            page.must_have_selector(".label.alert", text: "Closed")
+         end
+      end
    end
 end
--- test/support/helpers.rb
+++ test/support/helpers.rb
@@ -4,3 +4,28 @@ def sign_in(user)
    fill_in "Password", with: 'password'
    click_button "Sign in"
 end
+
+def sign_out
+   click_link "Sign out"
+end
+
+def bid_from_form(amount)
+   within("#bid-form-label + form") { find("#bid_amount").set(amount) }
+   click_button "Place bid"
+end
+
+def bid_increment_button
+   find("#increment-button")
+end
+
+def click_increment_button
+   click_button "increment-button"
+end
+
+def increment_button_amount
+   find(".increment-button-amount").value.to_d
+end
+
+def click_buyout_button
+   click_button "Buy Out"
+end
